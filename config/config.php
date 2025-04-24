06db63ffd2522c5be6a04b88fd42036463e0e6b7
Merroji <156344132+merroji@users.noreply.github.com>
2025-04-24 22:05:38 +0200

    Update: Login v2 - Hosting

--- config/config.php
+++ config/config.php
@@ -1,7 +1,12 @@
 <?php
+
+/*-- Archivos Config BD --*/
+    define('CONFIG_BD1_LOGIN','config/configBD1login_User12.php'); // Configuración de la base de datos para login
+    define('CONFIG_BD2_APP','config/configBD2app_User12.php'); // Configuración de la base de datos para app
+
     /*-- Rutas --*/
 
     /*-- Parámetros por defecto--*/
-    define('DEFAULT_CONTROLADOR','Juego'); //Controlador por defecto, el que llama a pais
-    define('DEFAULT_ACCION','bienvenida'); //Metodo por defecto, el que despliega el listado de paises
+    define('DEFAULT_CONTROLADOR',''); // Controlador por defecto
+    define('DEFAULT_ACCION',''); // Método por defecto
 ?>
