4a865d12ef732c75a083affc7a631dbb25816265
joseluisdelvalle <jl.valle@vitaly.es>
2025-04-25 08:30:21 +0200

    UPDATE: Index.php (sin completar) y routing

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
     
     // Unico archivo publico desde la web
+
+    // Cargar el autoload de composer
+    //require_once __DIR__ . '/../vendor/autoload.php';
+
+    // Cargar las rutas de la API
+    $routes = require_once __DIR__ . '/../config/routes.php';
+
+    // Obtener la ruta y metodo de la petición
+    $method = $_SERVER['REQUEST_METHOD'];
+    $path = $_SERVER['REQUEST_URI'];
+
+    // Limpiamos la ruta de headers
+    $requestUri = explode('?', $path)[0];
+
+    echo $requestUri;
     
+
 ?>
