7033489454802e631fc3a3d855b49de6bbd7283c
joseluisdelvalle <jl.valle@vitaly.es>
2025-04-25 12:25:46 +0200

    FIX: Test repository temp

--- conexionTmpBD/conexion.php
+++ conexionTmpBD/conexion.php
@@ -1,9 +1,9 @@
 <?php
     include '../config/configdb.php';
     //Conecta con la base de datos del servidor($conexión)
-    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); //Conecta con la base de datos
-    $conexion->set_charset("utf8"); //Usa juego caracteres UTF8
+    $this->conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); //Conecta con la base de datos
+    $this->conexion->set_charset("utf8"); //Usa juego caracteres UTF8
     //Desactivar errores
-    $controlador = new mysqli_driver();
-    $controlador->report_mode = MYSQLI_REPORT_OFF;
+    $this->controlador = new mysqli_driver();
+    $this->controlador->report_mode = MYSQLI_REPORT_OFF;
 ?>
--- src/repository/periodo-reservas-repository.php
+++ src/repository/periodo-reservas-repository.php
@@ -1,15 +1,20 @@
 <?php
 
-    include '../conexionTmpBD/conexion.php';
+    
 
     class PeriodoReservasRepository {
+
+        private $conexion;
+        private $controlador;
         
-        public function __construct() {}
+        public function __construct() {
+            require_once '../conexionTmpBD/conexion.php';
+        }
 
         public function getPeriodoActual() {
             // Solo tiene 1 registro que lo podemos obtener o modificar
             $sql = "SELECT * FROM periodo_reservas";
-            $resultado = $conexion->query($sql);
+            $resultado = $this->conexion->query($sql);
 
             if($resultado->num_rows > 0) {
                 return $resultado->fetch_assoc();
