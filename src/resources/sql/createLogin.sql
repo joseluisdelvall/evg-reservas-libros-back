50304afcec73c1b06b3ef66d1d2eda49648d2188
Celia <celiamorunoherrojo@gmail.com>
2025-04-30 00:10:36 +0200

    UPDATE: Eliminar columna idRol de la tabla USUARIO y ajustar las inserciones de datos

--- src/resources/sql/createLogin.sql
+++ src/resources/sql/createLogin.sql
@@ -27,10 +27,8 @@ CREATE TABLE IF NOT EXISTS USUARIO (
     idUsuario INT AUTO_INCREMENT NOT NULL,
     nombre VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    idRol TINYINT NOT NULL,
     CONSTRAINT usuario_pk PRIMARY KEY (idUsuario),
-    CONSTRAINT usuario_email_uq UNIQUE (email),
-    CONSTRAINT usuario_rol_fk FOREIGN KEY (idRol) REFERENCES ROL (idRol)
+    CONSTRAINT usuario_email_uq UNIQUE (email)
 );
 
 --
@@ -42,3 +40,31 @@ CREATE TABLE IF NOT EXISTS USUARIO_ROL (
     CONSTRAINT usuario_rol_usuario_fk FOREIGN KEY (idUsuario) REFERENCES USUARIO (idUsuario),
     CONSTRAINT usuario_rol_rol_fk FOREIGN KEY (idRol) REFERENCES ROL (idRol)
 );
+
+--
+-- Tabla APLICACION
+INSERT INTO APLICACION (nombre, descripcion, ruta, icono) VALUES ('Reserva de Libros', 'Aplicación para la reserva de libros', '/reservas', '/iconos/reserva.png');
+INSERT INTO APLICACION (nombre, descripcion, ruta, icono) VALUES ('Aula Matinal', 'Aplicación para la gestión del aula matinal', '/aula-matinal', '/iconos/aula-matinal.png');
+INSERT INTO APLICACION (nombre, descripcion, ruta, icono) VALUES ('Torneo Olímpico', 'Aplicación para la gestión del torneo olímpico', '/torneo-olimpico', '/iconos/torneo-olimpico.png');
+
+--
+-- Tabla ROL
+INSERT INTO ROL (nombre, descripcion, idAplicacion) VALUES ('RL', 'Reserva Libros', 1);
+INSERT INTO ROL (nombre, descripcion, idAplicacion) VALUES ('AM', 'Aula Matinal', 2);
+INSERT INTO ROL (nombre, descripcion, idAplicacion) VALUES ('TO', 'Torneo Olímpico', 3);
+
+-- Tabla USUARIO
+INSERT INTO USUARIO (nombre, email) VALUES ('Celia Moruno Herrojo', 'celiamorunoherrojo.guadalupe@alumnado.fundacionloyola.net');
+INSERT INTO USUARIO (nombre, email) VALUES ('Alberto Sánchez Díaz', 'albertosanchezdiaz.guadalupe@alumnado.fundacionloyola.net');
+INSERT INTO USUARIO (nombre, email) VALUES ('José Luís del Valle del Pino', 'joseluisdelvalledelpino.guadalupe@alumnado.fundacionloyola.net');
+
+-- Tabla USUARIO_ROL
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (1, 1);
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (1, 2);
+
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (2, 2);
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (2, 3);
+
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (3, 3);
+INSERT INTO USUARIO_ROL (idUsuario, idRol) VALUES (3, 1);
+
