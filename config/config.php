efeabf0c4978d370f47737b22f0a725051bb270d
asanchezEVG <asanchezdiaz.guadalupe@alumnado.fundacionloyola.net>
2025-05-12 16:24:59 +0200

    FIX: clean log git

--- config/config.php
+++ config/config.php
@@ -1,30 +1,8 @@
 <?php
 
-    $prod = true;
-
-    if ($prod) {
-        return [
-
-            /**
-             * CONFIG PRODUCTION
-             */
-
-            // Cambiar en caso de traslado, con / al final
-            'base_name' => 'evg-reservas-libros-back/',
-            'base_url' => 'https://12.2daw.esvirgua.com/',
-            'base_url_front' => 'https://12.2daw.esvirgua.com/'
-        ];
-    } else {
-        return [
-            /**
-             * CONFIG DEVELOPMENT
-             */
-
-            // Cambiar en caso de traslado, con / al final
-            'base_name' => 'evg-reservas-libros-back/',
-            'base_url' => 'http://localhost:8000/',
-            'base_url_front' => 'http://localhost:4200/'
-        ];
-    }
+    return [
+        // Cambiar en caso de traslado, con / al final
+        'base_name' => 'evg-reservas-libros-back/'
+    ];
 
 ?>
