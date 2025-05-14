9efb8c5e131a287ef208b020f7639860c8b418d5
joseluisdelvall <joseluisdelvall01@gmail.com>
2025-05-14 11:31:12 +0200

    FIX: deployment

--- config/config.php
+++ config/config.php
@@ -12,8 +12,8 @@
 
             // Cambiar en caso de traslado, con / al final
             'prod' => true,
-            'base_name' => 'evg-reservas-libros-back/',
-            'base_url' => 'https://12.2daw.esvirgua.com/evg-reservas-libros-back/',
+            'base_name' => ':NOMBRECARPETA/',
+            'base_url' => 'https://12.2daw.esvirgua.com/:NOMBRECARPETA/',
             'base_url_front' => 'https://12.2daw.esvirgua.com/evg-reservas-libros-front/',
             'google_client_id' => '660176374148-klpm52u3brlqsmpjvqci3ruk5qk1ofnl.apps.googleusercontent.com',
             'id_aplicacion' => '1',
