e47784b7020b71520208a19451d6fef4337afc42
joseluisdelvall <joseluisdelvall01@gmail.com>
2025-06-04 19:36:25 +0200

    FIX: libro recibido correo

--- src/repository/pedidos-repository.php
+++ src/repository/pedidos-repository.php
@@ -288,7 +288,7 @@ class PedidosRepository {
                     $emailService->sendEmail(
                         $row['correo'],
                         '¡Su libro ya está disponible para recoger!',
-                        'recibido-confirmacion',
+                        'libroRecibido',
                         $datosEmail,
                         $row['nombreAlumno'] . ' ' . $row['apellidosAlumno']
                     );
